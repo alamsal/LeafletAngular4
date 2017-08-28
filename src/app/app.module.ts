60e9843b42a1042fc6108234c66882be5840a9be
Aashis Lamsal <lamsalwork@gmail.com>
2017-08-28 15:04:35 -0600

    Added map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ClimatemapComponent } from './climatemap/climatemap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClimatemapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/climatemap/climatemap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ecol-viewer-climatemap',
+  templateUrl: './climatemap.component.html',
+  styleUrls: ['./climatemap.component.scss']
+})
+export class ClimatemapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
