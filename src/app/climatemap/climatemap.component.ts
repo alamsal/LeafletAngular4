e75eab5583a06d42050b40bc41b87360c9a32bee
Aashis Lamsal <lamsalwork@gmail.com>
2017-08-30 15:07:53 -0600

    Leaflet displayed in webpage

--- src/app/climatemap/climatemap.component.ts
+++ src/app/climatemap/climatemap.component.ts
@@ -3,25 +3,38 @@ import * as L from 'leaflet';
 
 @Component({
   selector: 'ecol-viewer-climatemap',
-  templateUrl: './climatemap.component.html',
+  template: `<h1>{{leafletTestText}}</h1>
+  <div id="leafletmap" style="width: 600px; height: 600px;"></div>`,
   styleUrls: ['./climatemap.component.scss']
 })
 export class ClimatemapComponent implements OnInit {
-  leafletTestText = 'tester3';
-  map = new L.Map('leafletmap');
-  osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  
 
   constructor() {
 
   }
 
   ngOnInit() {
+
+    const osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const  osm = new L.TileLayer(osmUrl, {minZoom: 3 , maxZoom: 12});
     
-    const osm = new L.TileLayer(this.osmUrl, {minZoom: 3 , maxZoom: 12});
-    this.map.setView(new L.LatLng(51, 0.7), 9);
-  
-    this.map.addLayer(osm);
+    const map = L.map('leafletmap', {
+      zoomControl: false,
+      center: L.latLng(35.29, -112),
+      zoom: 5,
+      minZoom: 4,
+      maxZoom: 12,
+      layers: [osm]
+      });
+
+
   }
+   leafletTestText = 'tester3';
+
+
+   
+  
 
 
   
